004db5161b28b9b755adde9c2407c60d55ae8b5f
Hasindu Senarathna <85918275+hasiya2004@users.noreply.github.com>
2023-06-12 13:51:32 +0530

    Update moon.java
    i changes some code here for get response  from space moon (:

--- moon.java
+++ moon.java
@@ -10,14 +10,14 @@ public class moon {
             String apiKey = "YOUR_NASA_API_KEY";
             String apiUrl = "https://api.nasa.gov/planetary/apod?api_key=" + apiKey;
 
-            // Send HTTP GET request to the API
+          
             URL url = new URL(apiUrl);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
 
             int responseCode = connection.getResponseCode();
             if (responseCode == HttpURLConnection.HTTP_OK) {
-                // Read the API response
+             
                 BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 String line;
                 StringBuilder response = new StringBuilder();
@@ -26,9 +26,7 @@ public class moon {
                 }
                 reader.close();
 
-                // Parse the JSON response
-                // Extract and display the moon information
-                // Here you can implement your own logic to extract the desired information from the API response
+                
                 System.out.println("Moon Information:");
                 System.out.println(response.toString());
             } else {
